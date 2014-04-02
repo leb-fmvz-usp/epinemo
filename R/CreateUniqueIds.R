8c2a63eb0057218a17dabe0d7cb936d5ae6b642e
grisi <grisi@leblaptop.(none)>
2014-04-01 23:54:10 -0300

    uniqueids

--- R/CreateUniqueIds.R
+++ R/CreateUniqueIds.R
@@ -1,8 +1,8 @@
 #' Create Unique Identifiers
 #' @description Create unique identifiers in a network presented in edgelist form
 #' @param data Edgelist of a network. \code{\link{data.frame}}
-#' @param from \code{\link{numeric}} \code{\link{vector}}, with the column(s) used to characterize the origin node of each link
-#' @param to \code{\link{numeric}} \code{\link{vector}}, with the column(s) used to characterize each the destiny node of each link
+#' @param from \code{\link{character}} \code{\link{vector}}, indicate the column(s) used to characterize the origin node of each link
+#' @param to \code{\link{character}} \code{\link{vector}}, indicate the column(s) used to characterize each the destiny node of each link
 #' @return Edgelist with new ID's, ranging from 1:n
 #' @details Used to be slow, now is very fast!
 #' @references None.

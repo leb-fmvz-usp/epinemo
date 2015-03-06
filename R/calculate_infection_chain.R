193adc2a7b970b8b94b72d2537be53200ae05022
fernandosm <fernandosix@gmail.com>
2015-03-05 21:31:35 -0300

    indentation, removing packages loading and inclusion of stopCluster function

--- R/calculate_infection_chain.R
+++ R/calculate_infection_chain.R
@@ -1,42 +1,74 @@
-#' It calculates infection chain.
-#' @description Parallel function to calculate outgoing and ingoing infection chain.
-#' @param Data \code{\link{data.frame}} with network information : node ID, origin node, destiny node, and the time in which the link was established.
-#' @param from \code{\link{character}}, indicates the column used to characterize the origin node of each link.
-#' @param to \code{\link{character}}, indicates the column used to characterize the destiny node of each link.
-#' @param Time \code{\link{character}}, indicates the column used to characterize the time in which the link was established.
-#' selected.nodes \code{\link{vector}}, the infection chain will be calculated for the nodes in the selected.node vector.
-#' type \code{\link{character}}, it determines which infection chain type will be calculated (default is 'outgoing'). Options are: 'outgoing', 'ingoing' or 'both'. 
-#' number.of.cores \code{\link{integer}}, number of cores used to calculate the infection chain (default is 2).
-#' close.connection \code{\link{character}}, whether the function closes all connections (default is TRUE). 
-#' @details This is a function that calculates the infection chain of a dynamic network.
-#' @return \code{\link{data.frame}}. The first (or first and second) column, \code{*$infection.chain}, is the infection chain value. The last column, \code{*$selected.nodes}, are the selected nodes.
+#' @title Calculates infection chain.
+#' 
+#' @description Parallel function to calculate outgoing and ingoing infection
+#'              chain.
+#' 
+#' @param Data \code{\link{data.frame}} with network information : node ID, origin
+#'        node, destiny node, and the time in which the link was established.
+#' 
+#' @param from \code{\link{character}}, indicates the column used to characterize
+#'        the origin node of each link.
+#' 
+#' @param to \code{\link{character}}, indicates the column used to characterize
+#'        the destiny node of each link.
+#' 
+#' @param Time \code{\link{character}}, indicates the column used to characterize
+#'        the time in which the link was established.
+#' 
+#' @param selected.nodes \code{\link{vector}}, the infection chain will be calculated
+#'        for the nodes in the selected.node vector.
+#' 
+#' @param type \code{\link{character}}, it determines which infection chain type
+#'        will be calculated (default is 'outgoing'). Options are: 'outgoing',
+#'        'ingoing' or 'both'. 
+#' 
+#' @param number.of.cores \code{\link{integer}}, number of cores used to calculate
+#'        the infection chain (default is 2).
+#' 
+#' @details This is a function that calculates the infection chain of a dynamic
+#'          network.
+#' 
+#' @return \code{\link{data.frame}}. The first (or first and second) column,
+#'         \code{*$infection.chain}, is the infection chain value. The last column,
+#'         \code{*$selected.nodes}, are the selected nodes.
+#' 
 #' @references 
-#' [1] K Buttner, J Krieter, and I Traulsen. “Characterization of Contact Structures for the Spread of Infectious Diseases in a Pork Supply Chain in Northern Germany by Dynamic Network Analysis of Yearly and Monthly Networks.” In: Transboundary and emerging diseases 2000 (May 2013), pp. 1–12.
-#' [2] C Dube, C Ribble, D Kelton, et al. “Comparing network analysis measures to determine potential epidemic size of highly contagious exotic diseases in fragmented monthly networks of dairy cattle movements in Ontario, Canada.” In: Transboundary and emerging diseases 55.9-10 (Dec. 2008), pp. 382–392.
-#' [3] C Dube, C Ribble, D Kelton, et al. “A review of network analysis terminology and its application to foot-and-mouth disease modelling and policy development.” In: Transboundary and emerging diseases 56.3 (Apr. 2009), pp. 73–85.
-#' [4] Jenny Frossling, Anna Ohlson, Camilla Bj ̈orkman, et al. “Application of network analysis parameters in risk-based surveillance - Examples based on cattle trade data and bovine infections in Sweden.” In:  Preventive veterinary medicine 105.3 (July 2012), pp. 202–208. doi: 10.1016/j.prevetmed.2011.12.011.
-#' [5] Maria Noremark, Nina Ha kansson, Susanna Sternberg Lewerin, et al. “Network analysis of cattle and pig movements in Sweden: measures relevant for disease control and risk based surveillance.” In: Preventive veterinary medicine 99.2-4 (2011), pp. 78–90. doi: 10.1016/j.prevetmed.2010.12.009.
+#' [1] K Buttner, J Krieter, and I Traulsen. “Characterization of Contact Structures
+#'     for the Spread of Infectious Diseases in a Pork Supply Chain in Northern
+#'     Germany by Dynamic Network Analysis of Yearly and Monthly Networks.” In:
+#'     Transboundary and emerging diseases 2000 (May 2013), pp. 1–12.
+#' [2] C Dube, C Ribble, D Kelton, et al. “Comparing network analysis measures to
+#'     determine potential epidemic size of highly contagious exotic diseases in
+#'     fragmented monthly networks of dairy cattle movements in Ontario, Canada.”
+#'     In: Transboundary and emerging diseases 55.9-10 (Dec. 2008), pp. 382–392.
+#' [3] C Dube, C Ribble, D Kelton, et al. “A review of network analysis terminology
+#'     and its application to foot-and-mouth disease modelling and policy development.”
+#'     In: Transboundary and emerging diseases 56.3 (Apr. 2009), pp. 73–85.
+#' [4] Jenny Frossling, Anna Ohlson, Camilla Bj ̈orkman, et al. “Application of
+#'     network analysis parameters in risk-based surveillance - Examples based
+#'     on cattle trade data and bovine infections in Sweden.” In:  Preventive
+#'     veterinary medicine 105.3 (July 2012), pp. 202–208. doi: 10.1016/j.prevetmed.2011.12.011.
+#' [5] Maria Noremark, Nina Ha kansson, Susanna Sternberg Lewerin, et al.
+#'     “Network analysis of cattle and pig movements in Sweden: measures relevant
+#'     for disease control and risk based surveillance.” In: Preventive veterinary
+#'     medicine 99.2-4 (2011), pp. 78–90. doi: 10.1016/j.prevetmed.2010.12.009.
+#' 
 #' \url{http://github.com/leb-fmvz-usp.github/epinemo}
-#' @export
+#' 
 #' @examples 
 #' # Loading data from....
 #' 
 #' # call infection chain function
-#' outgoing.infection.chai <- CalculateInfectionChain(Data, from, to, Time,
+#' outgoing.infection.chain <- CalculateInfectionChain(Data, from, to, Time,
 #'                                                    selected.nodes,
 #'                                                    type = 'outgoing')
 #'                                                    
-#' ingoing.infection.chai <- CalculateInfectionChain(Data, from, to, Time,
+#' ingoing.infection.chain <- CalculateInfectionChain(Data, from, to, Time,
 #'                                                    selected.nodes,
 #'                                                    type = 'outgoing')
 
 CalculateInfectionChain <- function(Data, from, to, Time, selected.nodes,
-                                    type = 'outgoing', number.of.cores = 2,
-                                    close.connection = TRUE){
-  
-  #### libraries #### 
-  require(foreach)
-  require(doSNOW)
+                                    type = 'outgoing', number.of.cores = NULL){
   
   #### Extracting, trasforming and loading the data base #####
   Data <- Data[,c(from,to,Time)]
@@ -107,17 +139,19 @@ CalculateInfectionChain <- function(Data, from, to, Time, selected.nodes,
   }
   
   #### Parallel call ####
-  registerDoSNOW(makeCluster(number.of.cores, type = "SOCK")); # creating connection
+  if(number.of.cores == NULL) number.of.cores <- parallel::detectCores()
+  cl <- parallel::makeCluster(number.of.cores, type = "SOCK")
+  doSNOW::registerDoSNOW(cl)
   
   if(type == 'outgoing'){
     
     mov.time <- sort(unique(Data[,Time]))
     
-    infection.chain <- foreach(n=1:length(selected.nodes[,'selected.nodes']),.verbose=FALSE, .combine = 'rbind',
+    infection.chain <- foreach(n=1:length(selected.nodes[,'selected.nodes']),
+                               .verbose=FALSE, .combine = 'rbind',
                                .inorder=TRUE) %dopar% (DoInfectionChain())
     
-    if(close.connection == TRUE)
-      closeAllConnections()
+    parallel::stopCluster(cl)
     
     infection.chain <- apply(infection.chain,1,sum)
     infection.chain <- data.frame(outgoing.infection.chain = infection.chain,
@@ -133,11 +167,11 @@ CalculateInfectionChain <- function(Data, from, to, Time, selected.nodes,
     names(Data)[b] <- to
     mov.time <- sort(unique(Data[,Time]), decreasing = T)
     
-    infection.chain <- foreach(n=1:length(selected.nodes[,'selected.nodes']),.verbose=FALSE, .combine = 'rbind',
+    infection.chain <- foreach(n=1:length(selected.nodes[,'selected.nodes']),
+                               .verbose=FALSE, .combine = 'rbind',
                                .inorder=TRUE) %dopar% (DoInfectionChain())
     
-    if(close.connection == TRUE)
-      closeAllConnections()
+    parallel::stopCluster(cl)
     
     infection.chain <- apply(infection.chain,1,sum)
     infection.chain <- data.frame(ingoing.infection.chain = infection.chain,
@@ -151,11 +185,11 @@ CalculateInfectionChain <- function(Data, from, to, Time, selected.nodes,
     mov.time2 <- sort(mov.time,decreasing = T)
     tamanho <- length(mov.time)
     
-    infection.chain <- foreach(n=1:length(selected.nodes[,'selected.nodes']),.verbose=FALSE, .combine = 'rbind',
+    infection.chain <- foreach(n=1:length(selected.nodes[,'selected.nodes']),
+                               .verbose=FALSE, .combine = 'rbind',
                                .inorder=TRUE) %dopar% (DoInfectionChain2())
     
-    if(close.connection == TRUE)
-      closeAllConnections()
+    parallel::stopCluster(cl)
     
     infection.chain <- data.frame(outgoing.infection.chain = infection.chain[,1],
                                   ingoing.infection.chain = infection.chain[,2],
